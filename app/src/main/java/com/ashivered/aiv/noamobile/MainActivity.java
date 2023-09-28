c82b317dfd3789053e98c78a4e2acc07bb67700f
Ashi Vered <76535872+AshiVered@users.noreply.github.com>
2023-09-28 10:59:15 +0300

    first commit

---
+++ app/src/main/java/com/ashivered/aiv/noamobile/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ashivered.aiv.noamobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import org.mozilla.geckoview.GeckoRuntime;
+import org.mozilla.geckoview.GeckoSession;
+import org.mozilla.geckoview.GeckoView;
+
+public class MainActivity extends AppCompatActivity {
+    private static GeckoRuntime sRuntime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GeckoView view = findViewById(R.id.geckoview);
+        GeckoSession session = new GeckoSession();
+        // Workaround for Bug 1758212
+        session.setContentDelegate(new GeckoSession.ContentDelegate() {});
+
+        if (sRuntime == null) {
+            // GeckoRuntime can only be initialized once per process
+            sRuntime = GeckoRuntime.create(this);
+        }
+
+        session.open(sRuntime);
+        view.setSession(session);
+        session.loadUri("https://noam.org.il"); // Or any other URL...
+        
+
+    }
+}
